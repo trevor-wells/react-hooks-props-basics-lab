59ca88ccc222bc79072d3ab85608c9e0933c7fa9
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-12 20:41:52 -0600

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
